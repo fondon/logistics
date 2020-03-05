9dc3b1e10be705a7e4abe2797cd892de12129f49
chd.y <chongdi.yang@outlook.com>
2020-03-05 18:56:02 +0800

    add license

---
+++ src/main/java/my/MeTest.java
@@ -0,0 +1,20 @@
+package my;
+
+import org.springframework.stereotype.Component;
+
+@Component("meTest")
+public class MeTest {
+    public MeTest() {
+
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("############# Yes Me scan ############");
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("######################################");
+        System.out.println("######################################");
+    }
+}
